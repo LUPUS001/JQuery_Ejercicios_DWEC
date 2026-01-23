4983f044f63e80064cee22fa90e3dff463ba3c90
LUPUS001 <antoniodaw23@gmail.com>
2026-01-23 12:13:33 +0000

    JQuery Anexo AJAX prototipo a revisar y corrección Mati

---
+++ JQuery_Ajax/AnexoAJAX/2/fecha.php
@@ -0,0 +1,6 @@
+<?php
+    // Simula que el servidor tarda 3 segundos en responder
+    sleep(3); 
+    // Devuelve la fecha
+    echo "Fecha del servidor: " . date("d-m-Y H:i:s");
+?>
---
+++ JQuery_Ajax/AnexoAJAX/3/codigo.js
@@ -0,0 +1,2 @@
+alert("¡Hola! Soy un script cargado dinámicamente vía AJAX.");
+console.log("Script ejecutado correctamente.");
